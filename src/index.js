231ffd07635aa4e9dd740bc20796723b9cec9c7c
111Yokoo <yokoomatheus@gmail.com>
2024-09-19 10:45:46 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import productRoutes from "./routes/productRouts.js";
+
+const app = express();
+app.use(express.json());
+app.use(productRoutes);
+
+app.listen(3000, () => {
+  console.log(`Servidor rodando na porta 3000`);
+});
