ca24b1f968dfa959ea445e5ba72ff26f19d35341
111Yokoo <yokoomatheus@gmail.com>
2024-09-20 09:03:49 -0300

    Inserção de dados e busca por id

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-import {getAllProducts} from "../services/productServices.js";
+import {createProduct, getAllProducts, getProductById} from "../services/productServices.js";
 
 export const getProducts = async(req, res) => {
     try{
@@ -8,6 +8,28 @@ export const getProducts = async(req, res) => {
     catch(error){
         res.status(500).json({
             error: "Erro ao buscar produtos",
-        })
+        });
+    }
+}
+
+export const createProductController = async(req, res) => {
+    try{
+        const newProduct = await createProduct();
+        res.status(201).json(newProduct);
+    }catch(error){
+        res.status(500).json({error: "Erro ao criar produto"});
+    }
+}
+
+export const getProduct = async(req, res) =>{
+    try{
+        const product = await getProductById(req.params.id);
+        if(product){
+            res.json(product);
+        }else{
+            req.status(500).json({error: "Produto não encontrado"});
+        }
+    }catch(error) {
+        req.status(500).json({error: "Erro ao buscar produto"});
     }
 }
